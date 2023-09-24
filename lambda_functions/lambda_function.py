50b03f61e6585644c03e5652ac899eab6eb36020
Eli Jaghab <elijaghab@gmail.com>
2023-09-24 14:21:41 -0400

    Refactor Selenium initialization and import dependencies

--- lambda_functions/lambda_function.py
+++ lambda_functions/lambda_function.py
@@ -5,40 +5,13 @@ from selenium.webdriver.chrome.service import Service
 from selenium import webdriver
 import os
 
-def list_files_in_directory(directory):
-    print(f"Listing files and directories in: {directory}")
-    print(directory)  # Include the root directory itself in the output
-    for root, dirs, files in os.walk(directory):
-        for file in files:
-            print(os.path.join(root, file))
-        for dir in dirs:
-            print(os.path.join(root, dir))
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
-# List files in the Lambda execution environment's root directory
-list_files_in_directory('/var/task')
-
-# List files in the Lambda execution environment's /opt directory
-list_files_in_directory('/opt')
-
-
-service = Service(executable_path='/opt/chromedriver')
-chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument("--headless")
-chrome_options.add_argument('--disable-gpu')
-chrome_options.add_argument('--no-sandbox')
-chrome_options.add_argument('--disable-dev-shm-usage')
-chrome_options.binary_location = '/opt/headless-chromium'    
-
-driver = webdriver.Chrome(service=service, options = chrome_options)
-driver.get('https://www.google.com/')
-
-driver.close()
-driver.quit()
-
-response = {
-    "statusCode": 200,
-    "body": "Selenium Headless Chrome Initialized"
-}
-
-extractor = Extractor()
-print(extractor.get_soundcloud_tracks())
+options = Options()
+options.add_argument('--disable-gpu')
+options.add_argument('--headless')
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+driver.get("http://example.com")
