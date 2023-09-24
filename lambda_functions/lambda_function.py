e4bbed6e0796a9b3045e90010bbc183b1ff057a9
Eli Jaghab <elijaghab@gmail.com>
2023-09-24 13:28:39 -0400

    Fix selenium options and driver setup

--- lambda_functions/lambda_function.py
+++ lambda_functions/lambda_function.py
@@ -7,11 +7,6 @@ from selenium.webdriver.chrome.options import Options
 
 service = Service(executable_path='/opt/headless-chromium')
 
-service.binary_location = '/opt/headless-chromium'
-service.add_argument('--headless')
-service.add_argument('--no-sandbox')
-service.add_argument('--single-process')
-service.add_argument('--disable-dev-shm-usage')
 
 driver = webdriver.Chrome(service=service)
 driver.get('https://www.google.com/')
