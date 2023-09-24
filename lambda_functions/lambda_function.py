8ed2c9fbdb447180ea01d26dad68cab82cc82cb2
Eli Jaghab <elijaghab@gmail.com>
2023-09-24 13:25:44 -0400

    Update selenium webdriver options and service in lambda_function.py

--- lambda_functions/lambda_function.py
+++ lambda_functions/lambda_function.py
@@ -5,19 +5,19 @@ from selenium.webdriver.chrome.service import Service
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
+service = Service(executable_path='/opt/headless-chromium')
 
-options = Options()
-options.binary_location = '/opt/headless-chromium'
-options.add_argument('--headless')
-options.add_argument('--no-sandbox')
-options.add_argument('--single-process')
-options.add_argument('--disable-dev-shm-usage')
+service.binary_location = '/opt/headless-chromium'
+service.add_argument('--headless')
+service.add_argument('--no-sandbox')
+service.add_argument('--single-process')
+service.add_argument('--disable-dev-shm-usage')
 
-driver = webdriver.Chrome('/opt/chromedriver',chrome_options=options)
+driver = webdriver.Chrome(service=service)
 driver.get('https://www.google.com/')
 
-driver.close();
-driver.quit();
+driver.close()
+driver.quit()
 
 response = {
     "statusCode": 200,
