6d730bfa3917afec4bec33f3aef4d708d58e0870
Eli Jaghab <elijaghab@gmail.com>
2023-09-24 13:20:58 -0400

    Initialize Selenium Headless Chrome

--- lambda_functions/lambda_function.py
+++ lambda_functions/lambda_function.py
@@ -2,14 +2,27 @@ from extractor import Extractor
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 
-driverpath = Service("/opt/chromedriver.exe") #add your own path
-try: 
-    driver = webdriver.Chrome(service=driverpath)
-except:
-    driverpath = Service("/opt/chromedriver") #add your own path
-    driver = webdriver.Chrome(service=driverpath)
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
+
+options = Options()
+options.binary_location = '/opt/headless-chromium'
+options.add_argument('--headless')
+options.add_argument('--no-sandbox')
+options.add_argument('--single-process')
+options.add_argument('--disable-dev-shm-usage')
+
+driver = webdriver.Chrome('/opt/chromedriver',chrome_options=options)
 driver.get('https://www.google.com/')
+
+driver.close();
+driver.quit();
+
+response = {
+    "statusCode": 200,
+    "body": "Selenium Headless Chrome Initialized"
+}
 
 extractor = Extractor()
 print(extractor.get_soundcloud_tracks())
