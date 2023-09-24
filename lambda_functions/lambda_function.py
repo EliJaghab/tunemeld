8ce34a30399160474d4dad3b335610e1ffb5c683
Eli Jaghab <elijaghab@gmail.com>
2023-09-23 23:03:59 -0400

    Fix import error and simplify webdriver initialization

--- lambda_functions/lambda_function.py
+++ lambda_functions/lambda_function.py
@@ -1,5 +1,13 @@
+
+try:
+    import requests
+    print("Successfully imported requests.")
+    raise ValueError
+except ImportError:
+    print("Failed to import requests.")
+    
+    
 from extractor import Extractor
 
 extractor = Extractor()
 print(extractor.get_soundcloud_tracks())
-print("trigger action")
