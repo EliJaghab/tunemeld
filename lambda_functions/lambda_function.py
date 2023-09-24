fd8bebbc9eb102796137e72c12d1d4816b8f23da
Eli Jaghab <elijaghab@gmail.com>
2023-09-24 12:33:57 -0400

    Remove unnecessary code and import statement

--- lambda_functions/lambda_function.py
+++ lambda_functions/lambda_function.py
@@ -1,12 +1,3 @@
-
-try:
-    import requests
-    print("Successfully imported requests.")
-    raise ValueError
-except ImportError:
-    print("Failed to import requests.")
-    
-    
 from extractor import Extractor
 
 extractor = Extractor()
