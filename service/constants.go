85e3504823876de5bad54974545536a212ba2da1
Eli Jaghab <elijaghab@gmail.com>
2023-10-21 23:33:34 -0400

    update get and write function

--- service/constants.go
+++ service/constants.go
@@ -1,32 +1,64 @@
 package service
 
-const (
-    
-    AppleMusicEDM = "https%3A%2F%2Fmusic.apple.com%2Fus%2Fplaylist%2Fdancexl%2Fpl.6bf4415b83ce4f3789614ac4c3675740"
-    SoundCloudEDM = "https%3A%2F%2Fsoundcloud.com%2Fsoundcloud-the-peak%2Fsets%2Fon-the-up-new-edm-hits"
-    SpotifyEDM = "37i9dQZF1DX4Wsb4d7NKfP"
-)
-
 type ServiceConfig struct {
-	BaseURL string
-	Host    string
-	CachedFilePath string
+	BaseURL  string
+	Host     string
+	ParamKey string // New field to hold the parameter key
 }
 
 var (
 	AppleMusicConfig = ServiceConfig{
-		BaseURL: "https://apple-music24.p.rapidapi.com/playlist1/?url=",
-		Host:    "apple-music24.p.rapidapi.com",
-		CachedFilePath: "playlist_data/apple_music_cache.json",
+		BaseURL:  "https://apple-music24.p.rapidapi.com/playlist1/",
+		Host:     "apple-music24.p.rapidapi.com",
+		ParamKey: "url", // Specify the parameter key for each service
 	}
 	SoundCloudConfig = ServiceConfig{
-		BaseURL: "https://soundcloud-scraper.p.rapidapi.com/v1/playlist/metadata?playlist=",
-		Host:    "soundcloud-scraper.p.rapidapi.com",
-		CachedFilePath: "playlist_data/soundcloud_music_cache.json",
+		BaseURL:  "https://soundcloud-scraper.p.rapidapi.com/v1/playlist/tracks",
+		Host:     "soundcloud-scraper.p.rapidapi.com",
+		ParamKey: "playlist",
 	}
 	SpotifyConfig = ServiceConfig{
-		BaseURL: "https://spotify23.p.rapidapi.com/playlist/?id=",
-		Host:    "spotify23.p.rapidapi.com",
-		CachedFilePath: "playlist_data/spotify_music_cache.json",
+		BaseURL:  "https://spotify23.p.rapidapi.com/playlist_tracks/",
+		Host:     "spotify23.p.rapidapi.com",
+		ParamKey: "id",
+	}
+)
+
+type PlaylistConfig struct {
+	PlaylistParam  string
+	CachedFilePath string
+	ServiceConfig
+	RequestOptions *RequestOptions
+}
+
+type RequestOptions struct {
+	Offset int
+	Limit  int
+}
+
+var (
+	AppleMusicEDMConfig = PlaylistConfig{
+		PlaylistParam:  "https%3A%2F%2Fmusic.apple.com%2Fus%2Fplaylist%2Fdancexl%2Fpl.6bf4415b83ce4f3789614ac4c3675740",
+		CachedFilePath: "playlist_data/apple_music_edm.json",
+		ServiceConfig:  AppleMusicConfig,
+	}
+	SoundCloudEDMConfig = PlaylistConfig{
+		PlaylistParam:  "https%3A%2F%2Fsoundcloud.com%2Fsoundcloud-the-peak%2Fsets%2Fon-the-up-new-edm-hits",
+		CachedFilePath: "playlist_data/soundcloud_edm.json",
+		ServiceConfig:  SoundCloudConfig,
+	}
+	SpotifyEDMConfig = PlaylistConfig{
+		PlaylistParam:  "37i9dQZF1DX4Wsb4d7NKfP",
+		CachedFilePath: "playlist_data/spotify_edm.json",
+		ServiceConfig:  SpotifyConfig,
+		RequestOptions: &RequestOptions{Offset: 0, Limit: 100},
+	}
+)
+
+var (
+	PlaylistConfigs = []PlaylistConfig{
+		AppleMusicEDMConfig,
+		SoundCloudEDMConfig,
+		SpotifyEDMConfig,
 	}
 )
---
+++ service/json_utils.go
@@ -0,0 +1,57 @@
+package service
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+)
+
+func getJSONfromBytes(data []byte) (map[string]interface{}, error) {
+    var result map[string]interface{}
+    err := json.Unmarshal(data, &result)
+    if err != nil {
+        return nil, fmt.Errorf("error parsing JSON: %w", err)
+    }
+    return result, nil
+}
+
+func getJSONfromFile(filename string) (map[string]interface{}, error) {
+	data, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+
+	var result map[string]interface{}
+	err = json.Unmarshal(data, &result)
+	if err != nil {
+		return nil, err
+	}
+
+	return result, nil
+}
+
+func WriteJSONToFile(data map[string]interface{}, filePath string) error {
+	jsonData, err := json.MarshalIndent(data, "", "  ")
+	if err != nil {
+		return fmt.Errorf("error marshalling JSON: %w", err)
+	}
+
+	dir := filepath.Dir(filePath)
+	if _, err := os.Stat(dir); os.IsNotExist(err) {
+		err = os.MkdirAll(dir, 0755)
+		if err != nil {
+			return fmt.Errorf("error creating directory: %w", err)
+		}
+	}
+
+	err = os.WriteFile(filePath, jsonData, 0644)
+	if err != nil {
+		return fmt.Errorf("error writing file: %w", err)
+	}
+
+	log.Printf("Successfully wrote JSON data to %s", filePath)
+
+	return nil
+}
