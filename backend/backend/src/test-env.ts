789faa5d1841ef6a99f564455b3404162bff6ce4
Eli Jaghab <elijaghab@gmail.com>
2024-08-16 18:14:30 -0400

    schedule historical view count

--- backend/backend/src/test-env.ts
+++
@@ -1,6 +0,0 @@
-import { config } from 'dotenv';
-
-config();
-
-console.log('MONGO_DATA_API_ENDPOINT:', process.env.MONGO_DATA_API_ENDPOINT);
-console.log('MONGO_DATA_API_KEY:', process.env.MONGO_DATA_API_KEY);
