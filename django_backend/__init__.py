02c25654203dad7efa6bbd694b0b18abc99cae8a
Eli Jaghab <elijaghab@gmail.com>
2024-08-30 22:25:11 -0400

    add new collections

--- django_backend/__init__.py
+++ django_backend/__init__.py
@@ -4,5 +4,7 @@ from django.conf import settings
 mongo_client = MongoClient(settings.MONGO_URI)
 mongo_db = mongo_client[settings.MONGO_DB_NAME]
 
+transformed_playlists_collection = mongo_db['transformed_playlists']
+raw_playlists_collection = mongo_db['raw_playlists']
 playlists_collection = mongo_db['view_counts_playlists']
 track_views_collection = mongo_db['historical_track_views']
