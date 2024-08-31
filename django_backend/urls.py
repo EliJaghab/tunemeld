bb456b64248a9a3a4528d4ef3a8afb34fb016472
Eli Jaghab <elijaghab@gmail.com>
2024-08-30 22:25:21 -0400

    add url endpoints

--- django_backend/urls.py
+++ django_backend/urls.py
@@ -23,4 +23,9 @@ urlpatterns = [
     path('', views.root, name='root'), 
     path("admin/", admin.site.urls),
     path('graph-data/<str:genre_name>/', views.get_graph_data, name='get_graph_data_by_genre'),
+    path('playlist-data/<str:genre_name>/', views.get_playlist_data, name='get_playlist_data_by_genre'),
+    path('service-playlist/<str:genre_name>/<str:service_name>/', views.get_service_playlist, name='get_service_playlist_by_genre_and_service'),
+    path('last-updated/<str:genre_name>/', views.get_last_updated, name='get_last_updated_by_genre'),
+    path('header-art/<str:genre_name>/', views.get_header_art, name='get_header_art_by_genre'),
 ]
+
