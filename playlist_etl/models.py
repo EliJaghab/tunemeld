5f820a436de910fa71e42a191435990b5c34dd41
Eli Jaghab <elijaghab@gmail.com>
2024-11-22 22:04:19 -0500

    format

--- playlist_etl/models.py
+++ playlist_etl/models.py
@@ -10,6 +10,7 @@ class GenreName(str, Enum):
     COUNTRY = "country"
     POP = "pop"
 
+
 class ServiceName(str, Enum):
     AGGREGATE = "Aggregate"
     SPOTIFY = "Spotify"
@@ -17,28 +18,35 @@ class ServiceName(str, Enum):
     SOUNDCLOUD = "SoundCloud"
     APPLE_MUSIC = "AppleMusic"
 
+
 class TrackData(BaseModel):
     service_name: ServiceName
     track_name: str | None = None
     artist_name: str | None = None
     track_url: HttpUrl | None = None
+
+
 class StartView(BaseModel):
     start_view_count: int
     start_timestamp: datetime
+
 
 class CurrentView(BaseModel):
     current_view_count: int
     current_timestamp: datetime
 
+
 class ServiceView(BaseModel):
     service_name: ServiceName
     start_view: StartView
     current_view: CurrentView
+
+
 class Track(BaseModel):
     isrc: str
     soundcloud_track_data: TrackData | None = None
     spotify_track_data: TrackData | None = None
-    apple_music_track_data: TrackData  | None = None
+    apple_music_track_data: TrackData | None = None
     album_cover_url: HttpUrl | None = None
     youtube_url: HttpUrl | None = None
     spotify_views: ServiceView | None = None
