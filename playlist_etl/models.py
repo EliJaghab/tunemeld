398e01c5601177c922a51be16b985506f15442a9
Eli Jaghab <elijaghab@gmail.com>
2024-11-22 21:57:23 -0500

    update models

--- playlist_etl/models.py
+++ playlist_etl/models.py
@@ -10,7 +10,6 @@ class GenreName(str, Enum):
     COUNTRY = "country"
     POP = "pop"
 
-
 class ServiceName(str, Enum):
     AGGREGATE = "Aggregate"
     SPOTIFY = "Spotify"
@@ -18,55 +17,41 @@ class ServiceName(str, Enum):
     SOUNDCLOUD = "SoundCloud"
     APPLE_MUSIC = "AppleMusic"
 
-
-class TrackServiceData(BaseModel):
-    spotify_track_name: str | None = None
-    spotify_artist_name: str | None = None
-    spotify_track_url: HttpUrl | None = None
-    soundcloud_track_name: str | None = None
-    soundcloud_artist_name: str | None = None
-    soundcloud_track_url: HttpUrl | None = None
-    apple_music_track_name: str | None = None
-    apple_music_artist_name: str | None = None
-    apple_music_track_url: HttpUrl | None = None
-    album_cover_url: HttpUrl | None = None
-    youtube_url: HttpUrl | None = None
-
-
+class TrackData(BaseModel):
+    service_name: ServiceName
+    track_name: str | None = None
+    artist_name: str | None = None
+    track_url: HttpUrl | None = None
 class StartView(BaseModel):
     start_view_count: int
     start_timestamp: datetime
-
 
 class CurrentView(BaseModel):
     current_view_count: int
     current_timestamp: datetime
 
-
 class ServiceView(BaseModel):
     service_name: ServiceName
     start_view: StartView
     current_view: CurrentView
-
-
-class View(BaseModel):
-    spotify_views: ServiceView
-    youtube_views: ServiceView
-
-
 class Track(BaseModel):
     isrc: str
-    service_data: TrackServiceData
-    view_count_data: View | None = None
+    soundcloud_track_data: TrackData | None = None
+    spotify_track_data: TrackData | None = None
+    apple_music_track_data: TrackData  | None = None
+    album_cover_url: HttpUrl | None = None
+    youtube_url: HttpUrl | None = None
+    spotify_views: ServiceView | None = None
+    youtube_views: ServiceView | None = None
 
 
 class TrackRank(BaseModel):
     isrc: str
     rank: int
-    sources: list[str]
+    sources: list[ServiceName]
 
 
-class PlaylistRank(BaseModel):
+class Playlist(BaseModel):
     service_name: ServiceName
     genre_name: str
     tracks: list[TrackRank]
