821f8212501f6d07d4dcc0d106f20cf81a6308bf
Eli Jaghab <elijaghab@gmail.com>
2024-11-24 16:49:39 -0500

    clean up aggregate2

--- playlist_etl/aggregate2.py
+++ playlist_etl/aggregate2.py
@@ -1,3 +1,16 @@
+from collections import defaultdict
+
+from playlist_etl.config import (
+    TRACK_PLAYLIST_COLLECTION,
+)
+from playlist_etl.config import RANK_PRIORITY
+from playlist_etl.models import TrackSourceServiceName, GenreName, PlaylistType
+from playlist_etl.mongo_db_client import MongoDBClient
+from playlist_etl.helpers import get_logger
+
+logger = get_logger(__name__)
+
+
 class Aggregate:
     def __init__(self, mongo_client: MongoDBClient):
         self.mongo_client = mongo_client
@@ -14,19 +27,14 @@ class Aggregate:
     def _group_by_genre(self, track_playlists) -> dict:
         """Group ISRC matches across the same genre from different services."""
         candidates_by_genre = defaultdict(lambda: defaultdict(dict))
-        for genre_name in GENRE_NAMES:
-            for service_name in SERVICE_NAMES:
+        for genre_name in GenreName:
+            for service_name in TrackSourceServiceName:
                 track_playlist = track_playlists.find_one(
                     {"genre_name": genre_name, "service_name": service_name}
                 )
-                if not track_playlist:
-                    logger.warning(
-                        f"No track playlist found for genre {genre_name} and service {service_name}"
-                    )
-                    continue
 
                 for track in track_playlist["tracks"]:
-                    candidates_by_genre[genre_name][track["isrc"]][track["service_name"]] = track[
+                    candidates_by_genre[genre_name][track["isrc"]][service_name] = track[
                         "rank"
                     ]
 
@@ -43,8 +51,8 @@ class Aggregate:
 
     def _add_aggregate_rank(self, matches: dict) -> dict:
         """Rank matches based on the rank of the services."""
-        for genre_name, isrc_sources in matches.items():
-            for isrc, sources in isrc_sources.items():
+        for isrc_sources in matches.values():
+            for sources in isrc_sources.values():
                 aggregate_service_name = None
 
                 for service_name in RANK_PRIORITY:
@@ -52,18 +60,15 @@ class Aggregate:
                         aggregate_service_name = service_name
                         break
 
-                if aggregate_service_name:
-                    raw_aggregate_rank = sources[aggregate_service_name]
-                    sources["raw_aggregate_rank"] = raw_aggregate_rank
-                    sources["aggregate_service_name"] = aggregate_service_name
+                raw_aggregate_rank = sources[aggregate_service_name]
+                sources["raw_aggregate_rank"] = raw_aggregate_rank
+                sources["aggregate_service_name"] = aggregate_service_name
 
         return matches
 
     def _rank_matches(self, matches: dict) -> dict:
-        for genre_name, isrc_sources in matches.items():
-            # Convert isrc_sources (dict) to list and sort
+        for isrc_sources in matches.values():
             sorted_sources = sorted(isrc_sources.values(), key=lambda x: x["raw_aggregate_rank"])
-            # Assign new rank
             for i, sources in enumerate(sorted_sources, start=1):
                 sources["rank"] = i
         return matches
@@ -75,12 +80,13 @@ class Aggregate:
                 {
                     "isrc": isrc,
                     "rank": sources["rank"],
-                    "sources": list(sources.keys()),  # Extracting service names
+                    "sources": sources
                 }
                 for isrc, sources in isrc_sources.items()
             ]
+            tracks.sort(key=lambda x: x["rank"])
             formatted_playlists[genre_name] = {
-                "service_name": TrackSourceServiceName.AGGREGATE.value,
+                "service_name": PlaylistType.AGGREGATE.value,
                 "genre_name": genre_name,
                 "tracks": tracks,
             }
@@ -88,13 +94,11 @@ class Aggregate:
 
     def _write_aggregated_playlists(self, formatted_playlists: dict) -> None:
         logger.info("Writing aggregated playlists to MongoDB")
-        for genre_name in GENRE_NAMES:
-            playlist = formatted_playlists.get(genre_name)
-            if not playlist:
-                logger.warning(f"No aggregated playlist found for genre {genre_name}")
-                continue
+        for genre_name in GenreName:
+            genre_name = genre_name.value
+            playlist = formatted_playlists[genre_name]
             self.mongo_client.get_collection(TRACK_PLAYLIST_COLLECTION).update_one(
-                {"service_name": TrackSourceServiceName.AGGREGATE.value, "genre_name": genre_name},
+                {"service_name": PlaylistType.AGGREGATE.value, "genre_name": genre_name},
                 {"$set": playlist},
                 upsert=True,
             )
