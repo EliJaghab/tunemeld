52d550250d8f779c3d8bd4a3336acead33c08b49
Eli Jaghab <elijaghab@gmail.com>
2024-11-24 19:15:48 -0500

    Refactor new aggregate file

--- playlist_etl/aggregate2.py
+++ playlist_etl/aggregate2.py
@@ -2,9 +2,9 @@ from collections import defaultdict
 
 from playlist_etl.config import (
     TRACK_PLAYLIST_COLLECTION,
+    RANK_PRIORITY,
 )
-from playlist_etl.config import RANK_PRIORITY
-from playlist_etl.models import TrackSourceServiceName, GenreName, PlaylistType
+from playlist_etl.models import TrackSourceServiceName, GenreName, PlaylistType, TrackRank, Playlist
 from playlist_etl.mongo_db_client import MongoDBClient
 from playlist_etl.helpers import get_logger
 
@@ -26,79 +26,81 @@ class Aggregate:
 
     def _group_by_genre(self, track_playlists) -> dict:
         """Group ISRC matches across the same genre from different services."""
-        candidates_by_genre = defaultdict(lambda: defaultdict(dict))
+        candidates_by_genre = defaultdict(lambda: defaultdict(lambda: defaultdict(dict)))
         for genre_name in GenreName:
             for service_name in TrackSourceServiceName:
-                track_playlist = track_playlists.find_one(
+                playlist = track_playlists.find_one(
                     {"genre_name": genre_name, "service_name": service_name}
                 )
-
-                for track in track_playlist["tracks"]:
-                    candidates_by_genre[genre_name][track["isrc"]][service_name] = track[
-                        "rank"
-                    ]
+                track_ranks = [TrackRank(**track) for track in playlist["tracks"]]
+                for track in track_ranks:
+                    candidates_by_genre[genre_name][track.isrc]["sources"][service_name] = track.rank
 
         return candidates_by_genre
 
     def _get_matches(self, candidates: dict) -> dict:
         """Get ISRCs that come up more than once for the same genre."""
-        matches = defaultdict(dict)
+        matches = defaultdict(lambda: defaultdict(dict))
         for genre_name, isrcs in candidates.items():
-            for isrc, sources in isrcs.items():
-                if len(sources) > 1:
-                    matches[genre_name][isrc] = sources
+            for isrc, isrc_data in isrcs.items():
+                if len(isrc_data["sources"]) > 1:
+                    matches[genre_name][isrc]["sources"] = isrc_data["sources"]
         return matches
 
     def _add_aggregate_rank(self, matches: dict) -> dict:
         """Rank matches based on the rank of the services."""
-        for isrc_sources in matches.values():
-            for sources in isrc_sources.values():
+        for genre_name, isrcs in matches.items():
+            for isrc, isrc_data in isrcs.items():
                 aggregate_service_name = None
 
                 for service_name in RANK_PRIORITY:
-                    if service_name in sources:
+                    if service_name in isrc_data["sources"]:
                         aggregate_service_name = service_name
                         break
 
-                raw_aggregate_rank = sources[aggregate_service_name]
-                sources["raw_aggregate_rank"] = raw_aggregate_rank
-                sources["aggregate_service_name"] = aggregate_service_name
+                raw_aggregate_rank = isrc_data["sources"][aggregate_service_name]
+                isrc_data["raw_aggregate_rank"] = raw_aggregate_rank
+                isrc_data["aggregate_service_name"] = aggregate_service_name
 
         return matches
 
     def _rank_matches(self, matches: dict) -> dict:
-        for isrc_sources in matches.values():
-            sorted_sources = sorted(isrc_sources.values(), key=lambda x: x["raw_aggregate_rank"])
-            for i, sources in enumerate(sorted_sources, start=1):
-                sources["rank"] = i
+        for genre_name, isrcs in matches.items():
+            isrc_list = list(isrcs.values())
+            isrc_list.sort(key=lambda x: x["raw_aggregate_rank"])
+            for rank, isrc_data in enumerate(isrc_list, start=1):
+                isrc_data["rank"] = rank
         return matches
 
-    def _format_aggregated_playlist(self, matches_by_genre: dict) -> dict:
+    def _format_aggregated_playlist(self, sorted_matches: dict) -> list[dict]:
         formatted_playlists = {}
-        for genre_name, isrc_sources in matches_by_genre.items():
-            tracks = [
-                {
-                    "isrc": isrc,
-                    "rank": sources["rank"],
-                    "sources": sources
-                }
-                for isrc, sources in isrc_sources.items()
-            ]
-            tracks.sort(key=lambda x: x["rank"])
-            formatted_playlists[genre_name] = {
-                "service_name": PlaylistType.AGGREGATE.value,
-                "genre_name": genre_name,
-                "tracks": tracks,
-            }
+        for genre_name, isrcs in sorted_matches.items():
+            tracks = []
+            for isrc, isrc_data in isrcs.items():
+                track = TrackRank(
+                    isrc=isrc,
+                    rank=isrc_data["rank"],
+                    sources=isrc_data["sources"],
+                    raw_aggregate_rank=isrc_data["raw_aggregate_rank"],
+                    aggregate_service_name=isrc_data["aggregate_service_name"],
+                )
+                tracks.append(track.model_dump())
+                
+            playlist = Playlist(
+                service_name=PlaylistType.AGGREGATE,
+                genre_name=genre_name,
+                tracks=tracks,
+            )
+            formatted_playlists[genre_name] = playlist.model_dump()
         return formatted_playlists
 
     def _write_aggregated_playlists(self, formatted_playlists: dict) -> None:
         logger.info("Writing aggregated playlists to MongoDB")
         for genre_name in GenreName:
             genre_name = genre_name.value
-            playlist = formatted_playlists[genre_name]
+            playlist_dict = formatted_playlists[genre_name]
             self.mongo_client.get_collection(TRACK_PLAYLIST_COLLECTION).update_one(
                 {"service_name": PlaylistType.AGGREGATE.value, "genre_name": genre_name},
-                {"$set": playlist},
+                {"$set": playlist_dict},
                 upsert=True,
             )
